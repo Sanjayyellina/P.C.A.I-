01eb5475ea8e1189947bfc096cdf1ae08415f9e6
Abhianv sanjay yellina <abhinavsanjayyellina@gmail.com>
2026-07-30 01:06:18 -0500

    Add dataset manifest package

---
+++ src/pcai/datasets/__init__.py
@@ -0,0 +1,5 @@
+"""Dataset metadata contracts for reproducible P.C.A.I. experiments."""
+
+from pcai.datasets.manifest import CaptureGroup, DatasetSample, load_manifest
+
+__all__ = ["CaptureGroup", "DatasetSample", "load_manifest"]
